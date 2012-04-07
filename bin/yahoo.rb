c1666fd7aea3d1b113ac045f2f1c0dcf6c540da9
penumbra <penumbraposts@yahoo.com>
2012-04-07 10:33:42 -0400

    first commit

---
+++ bin/yahoo.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+lib_dir = File.expand_path('../../lib', __FILE__)
+$LOAD_PATH << lib_dir << '.'
+
+require 'yahoo'
+
+# uses lib/yahoo.rb to read SynchronicityPhenomena group content
+yahoo = Yahoo.new( 'penumbrareader@yahoo.com', 'PASSWORD_HERE')
+
+html = yahoo.select_group( 'SynchronicityPhenomena' )
+
+# Browse::Helper::save( 'data/synchronicity_phenomena.html', html ) unless html == nil
+
+(17277..20000).each do |id|
+  html = yahoo.get_message(id)
+
+  sleep( 10 )
+ 
+  if html != nil
+    filename = format("data/message-%05d.html", id)
+    puts "writing #{filename}..."
+    Browse::Helper::save( filename, html)
+  end
+end
