3fc48f82ff2e9a98f223b30b7a81dfb622c17471
penumbra <penumbraposts@yahoo.com>
2012-04-08 14:03:15 -0400

    renamed and added reprocessing feature

---
+++ lib/yahoo/reprocess.rb
@@ -0,0 +1,25 @@
+# Report an array of ID's for files listed in specified "reprocess file".
+# This represents downloaded files that contain "unavailable" messages
+#
+# Note: the file was created by redirecting grep output as follows:
+#   'grep "unavailable" * >reprocess.txt'
+#
+# Example Output:
+# message-28001.html:The message you requested is temporarily unavailable because this group has exceeded its download limit.
+# message-28001.html:The message you requested is temporarily unavailable because this group ...
+#
+class Reprocess
+  attr_reader :ids
+
+  def initialize( reprocess_fn )
+    f = File.open( reprocess_fn )
+
+    @ids = []
+    f.lines.each do |line|
+      val =  line[8..12]
+      @ids << val
+    end
+
+    f.close
+  end
+end
