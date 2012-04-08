f43938974a1c2e3caa84c1653af31be914723c09
penumbra <penumbraposts@yahoo.com>
2012-04-08 19:58:19 -0400

    update to save to disk

--- bin/topics.rb
+++ bin/topics.rb
@@ -6,7 +6,14 @@ $LOAD_PATH << lib_dir << '.'
 require 'yahoo'
 
 ConfigFile = '/projects/yahoo/yahoo_secret.yml'
+TopicsFile = '/projects/yahoo/data/topics.txt'
 
 tr = Yahoo::Topics::Runner.new( ConfigFile )
 
 tr.process_data
+
+f = File.open( TopicsFile, 'w' )
+
+tr.all_topics.sort.each {|t| f.puts t }
+
+f.close
