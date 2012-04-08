109abab110c7f1563e245b55b910d476915bb8a3
penumbra <penumbraposts@yahoo.com>
2012-04-08 14:48:06 -0400

    friendly version

--- bin/yahoo.rb
+++ bin/yahoo.rb
@@ -6,9 +6,9 @@ $LOAD_PATH << lib_dir << '.'
 require 'yahoo'
 
 # uses lib/yahoo.rb to read SynchronicityPhenomena group content
-yahoo_runner = Yahoo::Runner.new( '/projects/yahoo/yahoo_secret.yml' )
+yahoo_runner = Yahoo::Runner.new( '/projects/yahoo/yahoo.yml' )
 
-# download 35 messages (14342..14377)
-(14342..14343).each do |id|
+# download 1,000 messages
+(1..1000).each do |id|
   yahoo_runner.process_message( id )
 end
