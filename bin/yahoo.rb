cf57a7a51ff1bbc84510d3ff4fbf0cc89d15dbe3
penumbra <penumbraposts@yahoo.com>
2012-04-08 14:32:27 -0400

    update to use Yahoo module vs. class

--- bin/yahoo.rb
+++ bin/yahoo.rb
@@ -6,11 +6,9 @@ $LOAD_PATH << lib_dir << '.'
 require 'yahoo'
 
 # uses lib/yahoo.rb to read SynchronicityPhenomena group content
-yahoo_runner = YahooRunner.new( '/projects/yahoo/yahoo.yml' )
+yahoo_runner = Yahoo::Runner.new( '/projects/yahoo/yahoo_secret.yml' )
 
 # download 35 messages (14342..14377)
-(14342..14377).each do |id|
+(14342..14343).each do |id|
   yahoo_runner.process_message( id )
-  # wait 5 seconds so as to not cause a denial of service exception
-  sleep( 5 )
 end
