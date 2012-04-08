c624b0cd90cd817cdde9aa042d8a7fc9c7f0c645
penumbra <penumbraposts@yahoo.com>
2012-04-08 13:52:19 -0400

    removed reprocessing code

--- bin/yahoo.rb
+++ bin/yahoo.rb
@@ -4,17 +4,13 @@ lib_dir = File.expand_path('../../lib', __FILE__)
 $LOAD_PATH << lib_dir << '.'
 
 require 'yahoo'
-require 'limits'
 
 # uses lib/yahoo.rb to read SynchronicityPhenomena group content
-limits = Limits.new
-
 yahoo_runner = YahooRunner.new( '/projects/yahoo/yahoo.yml' )
 
-# 35 messages that were unavailable due to out (14342..14377)
-limits.ids.each do |id|
-  if id =~ /^28/
-    yahoo_runner.process_message( id )
-    sleep( 5 )
-  end
+# download 35 messages (14342..14377)
+(14342..14377).each do |id|
+  yahoo_runner.process_message( id )
+  # wait 5 seconds so as to not cause a denial of service exception
+  sleep( 5 )
 end
