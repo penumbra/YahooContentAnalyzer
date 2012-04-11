17cb2591b49f13e1165658bc3733cb7f671d7cda
penumbra <penumbraposts@yahoo.com>
2012-04-11 05:35:35 -0400

    use output path

--- lib/yahoo/content/info_extraction.rb
+++ lib/yahoo/content/info_extraction.rb
@@ -7,7 +7,7 @@ module Yahoo
       def initialize( yahoo_yml )
         prop = YAML::load_file( yahoo_yml )
 
-        @data_path = prop[Yahoo::Runner::YahooConfigTag]['data_path']
+        @output_path = prop[Yahoo::Runner::YahooConfigTag]['output_path']
 
         @amplify = Yahoo::Content::Amplify.new( yahoo_yml )
       end
@@ -22,19 +22,19 @@ module Yahoo
       end
 
       def show_results( doc )
-        Amplify::parse( Amplify::top_topics( doc ) ) do |tn, weight, values|
+        Parse::Amplify::parse( Parse::Amplify::top_topics( doc ) ) do |tn, weight, values|
           puts "[Top Topics => #{tn}, weight => #{weight}]"
           values.each {|val| val.each {|k,v| puts "#{k}=>#{v}"}}
         end
 
-        Amplify::parse( Amplify::proper_nouns( doc ) ) do |tn, weight, values|
+        Parse::Amplify::parse( Parse::Amplify::proper_nouns( doc ) ) do |tn, weight, values|
           puts "[Proper Noun => #{tn}, weight => #{weight}]"
           values.each {|val| val.each {|k,v| puts "#{k}=>#{v}"}}
         end
       end
 
       def save_results( nokogiri_doc, id )
-        fn = File::join( @data_path, id.sub('html', 'xml') )
+        fn = File::join( @output_path, id.sub('html', 'xml') )
         f = File.open(fn, 'w')
 
         nokogiri_doc.write_to( f, :indent => 2 )
