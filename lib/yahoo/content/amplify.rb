79d8690a5c9d516b6a9f5e932b188741886ba0d0
penumbra <penumbraposts@yahoo.com>
2012-04-11 04:05:53 -0400

    moved static methods to separate class

--- lib/yahoo/content/amplify.rb
+++ lib/yahoo/content/amplify.rb
@@ -5,6 +5,8 @@ require 'uri'
 module Yahoo
   module Content
     class Amplify
+      include Yahoo::Content::AmplifyParse
+
       ApiPort= 8180
       ApiHost= 'portaltnx20.openamplify.com'
       ApiPath= '/AmplifyWeb_v20/AmplifyThis'
@@ -24,42 +26,6 @@ module Yahoo
         response = @http.post(ApiPath, "apiKey=#{@api_key}&#{input_type}=#{URI::escape(msg)}")
 
         Nokogiri::XML( response.read_body )
-      end
-
-      def self.top_topics( doc )
-        doc.xpath("//TopTopics/TopicResult")
-      end
-
-      def self.proper_nouns( doc )
-        doc.xpath("//ProperNouns/TopicResult")
-      end
-
-      def self.parse( topic_results )
-        return if topic_results == nil
-
-        topic_results.each do |topic_result|
-          tn     = topic_result.search("Topic/Name/text()").to_s
-          weight = topic_result.search("Topic/Value/text()").to_s
-
-          # find NamedEntityType/Result info
-          values = self.get_named_entities( topic_result )
-          yield( tn, weight, values )
-        end
-      end
-
-      # returns an array of named entity information as hashes
-      def self.get_named_entities( topic_result )
-        result = []
-
-        # check NamedEntityType within each topic
-        entities = topic_result.search('NamedEntityType/Result')
-
-        entities.each do |entity|
-          result << {:entity => entity.search('Name/text()').to_s,
-                     :value => entity.search('Value/text()').to_s }
-        end
-
-        result
       end
     end
   end
