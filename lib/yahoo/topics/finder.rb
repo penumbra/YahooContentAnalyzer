033dd2e0571e957f4861090b745cfdb6a05a212a
penumbra <penumbraposts@yahoo.com>
2012-04-11 05:20:55 -0400

    renamed topic_finder to finder

--- lib/yahoo/topics/finder.rb
+++ lib/yahoo/topics/finder.rb
@@ -4,10 +4,11 @@ require 'nokogiri'
 
 module Yahoo
   module Topics
-    class TopicFinder < Yahoo::Finder
+    # locate a specific element by xpath within the specified HTML document
+    class Finder < Yahoo::Finder
       GroupTopicXPath = "//td[@class='ygrp-topic-title entry-title']"
 
-      def self.find_topic( fn )
+      def self.find( fn )
         find( fn, GroupTopicXPath )
       end
     end
