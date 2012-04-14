71734e6d68f8475133d9839d109d4e57cfca5b0e
penumbra <penumbraposts@yahoo.com>
2012-04-13 22:47:04 -0400

    refactored html parsing code

---
+++ lib/yahoo/content/parse/find_content.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'set'
+require 'nokogiri'
+
+module Yahoo
+  module Content
+    module Parse
+      # located an element by xpath within a specified HTML document
+      class FindContent < Yahoo::Finder
+        GroupContentXPath = "//div[@class='msgarea entry-content']"
+
+        def self.find( fn, xpath = GroupContentXPath )
+          node = super( fn, xpath )
+
+          node.text
+        end
+      end
+    end
+  end
+end
