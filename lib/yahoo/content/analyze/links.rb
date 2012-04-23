2298358d87162c95de7694defe7d0f6f56401e08
penumbra <penumbraposts@yahoo.com>
2012-04-23 00:11:06 -0400

    refactored analysis code

---
+++ lib/yahoo/content/analyze/links.rb
@@ -0,0 +1,27 @@
+module Yahoo
+  module Content
+    module Analyze
+      class Links
+        attr_reader :message_links
+        attr_reader :email_links
+
+        def initialize
+          @message_links = []
+          @email_links = []
+        end
+
+        def analyze( entry )
+          @message_links = Parse::FindLinks.find( entry )
+        end
+
+        def to_s
+          if @message_links
+            print "linked messages => "
+            @message_links.each {|t| print "#{t} "}
+            puts
+          end
+        end
+      end
+    end # Analyze
+  end # Content
+end # Yahoo
