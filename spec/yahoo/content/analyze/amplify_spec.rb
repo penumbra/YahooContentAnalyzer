769e8f77d0eef2676e779da04c14f23ab62fd12f
penumbra <penumbraposts@yahoo.com>
2012-04-22 23:30:10 -0400

    refactored content rspecs

---
+++ spec/yahoo/content/analyze/amplify_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+module Yahoo
+  module Content
+    module Analyze
+      describe Amplify do
+        let (:amplify) do
+          amplify = Amplify.new( Yahoo_Yml )
+        end
+
+        let(:msg) do
+          msg = Yahoo::Content::Parse::FindHtml.find_content( SampleHtml )
+        end
+
+        it "should have a top topic of dream with a weight of 20.00" do
+          amplify.analyze( msg )
+
+          Parse::Amplify::parse_topics( amplify.doc ) do |tn, wt, vals|
+            tn.should == "dream"
+            wt.should == "20.00"
+            break
+          end
+        end
+      end
+    end
+  end
+end
+
---
+++ spec/yahoo/content/analyze/zemanta_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+module Yahoo
+  module Content
+    module Analyze
+      describe Zemanta do
+
+        let(:zemanta) do
+          zemanta = Zemanta.new( Yahoo_Yml )
+        end
+
+        let(:msg) do
+          msg = Yahoo::Content::Parse::FindHtml.find_content( SampleHtml )
+        end
+
+        it "should list keywords" do
+          zemanta.analyze( msg )
+
+          Parse::Zemanta::parse_keywords( zemanta.doc ) do |kw|
+            kw.should == 'Lucid dream'
+            break
+          end
+        end
+      end
+    end
+  end
+end
