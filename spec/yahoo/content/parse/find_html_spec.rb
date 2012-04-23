7a4c8309d9a2fd830ca6e4a80119c2d4c8acfcbc
penumbra <penumbraposts@yahoo.com>
2012-04-22 22:54:54 -0400

    refactored

--- spec/yahoo/content/parse/find_html_spec.rb
+++ spec/yahoo/content/parse/find_html_spec.rb
@@ -4,19 +4,23 @@ module Yahoo
   module Content
     module Parse
       describe FindHtml do
-        Title = 'Lucid Dreams synch'
-        Date = '2002-2-11T16:26:25Z'
+        Title  = 'Lucid Dreams synch'
+        Date   = '2002-2-11T16:26:25Z'
         Author = 'Don Boulet'
 
         describe "#find_title" do
           it "should return a topic title" do
             FindHtml.find_title( SampleHtml ).should == Title
           end
+        end
 
+        describe "#find_date" do
           it "should return a specific date" do
             FindHtml.find_date( SampleHtml ).should == Date
           end
+        end
 
+        describe "#find_author" do
           it "should return a specific author" do
             FindHtml.find_author( SampleHtml ).should == Author
           end
