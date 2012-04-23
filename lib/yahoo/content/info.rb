ec148071567a531d9b39f587fd811d86e119bdb9
penumbra <penumbraposts@yahoo.com>
2012-04-23 00:32:54 -0400

    removed attr_readers

--- lib/yahoo/content/info.rb
+++ lib/yahoo/content/info.rb
@@ -2,23 +2,21 @@ module Yahoo
   module Content
     class Info < Yahoo::Shared::Config
       YamlHeader = "---"
-      attr_reader :amplify
-      attr_reader :zemanta
-      attr_reader :calais
 
       def initialize( yahoo_yml )
         super( yahoo_yml )
 
-        @amplify = Amplify.new( yahoo_yml )
-        @zemanta = Zemanta.new( yahoo_yml )
-        @calais  = Calais.new( yahoo_yml )
-        @links   = Links.new
+        @amplify = Analyze::Amplify.new( yahoo_yml )
+        @zemanta = Analyze::Zemanta.new( yahoo_yml )
+        @calais  = Analyze::Calais.new( yahoo_yml )
+        @links   = Analyze::Links.new
       end
 
       def analyze( entry )
-        @title = Parse::FindContent.find_title( entry )
-        @date  = Parse::FindContent.find_date( entry )
-        msg    = Parse::FindContent.find_msg( entry )
+        @title  = Parse::FindHtml.find_title( entry )
+        @date   = Parse::FindHtml.find_date( entry )
+        @author = Parse::FindHtml.find_author( entry )
+        msg     = Parse::FindHtml.find_content( entry )
 
         # perform information extraction using Open Amplify API
         @amplify.analyze( msg )
@@ -27,14 +25,14 @@ module Yahoo
         @zemanta.analyze( msg )
 
         # perform information extraction using Open Calais API
-        @calais.analyze( @title, msg, @date )
+        @calais.analyze( @title, @date, msg )
 
         # find related messages for this topic/title
         @links.analyze( entry )
       end
 
       def to_s
-        puts "title => #{@title}, date => #{@date}"
+        puts "title => #{@title}, date => #{@date}, author => #{@author}"
         puts YamlHeader
         @amplify.to_s if @amplify && @amplify.doc
         puts YamlHeader
