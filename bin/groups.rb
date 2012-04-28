6c281e9421ff2d3c39129f4cb266be9fe8690554
penumbra <penumbraposts@yahoo.com>
2012-04-28 13:08:01 -0400

    added range warning comment

--- bin/groups.rb
+++ bin/groups.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env /projects/rubinius-1.2.4/bin/ruby
 
 lib_dir = File.expand_path('../../lib', __FILE__)
 $LOAD_PATH << lib_dir << '.'
@@ -10,5 +10,11 @@ ConfigFile = '/projects/yahoo/yahoo.yml'
 # uses lib/yahoo.rb to read SynchronicityPhenomena group content
 yahoo_runner = Yahoo::Groups::Runner.new( ConfigFile )
 
+#
+# Warning: This program does not  yet prevent processing
+#  a message id range where the upper range exceed the
+#  total number of messages in a Yahoo Group Forum.
+#
+
 # download 1,000 messages from my favorite Yahoo Newsgroup
-(1..1000).each { |id| yahoo_runner.process_message( id ) }
+(32001..33000).each { |id| yahoo_runner.process_message( id ) }
