86bbe2f261761184209dfce2e9591c69e1a7f923
penumbra <penumbraposts@yahoo.com>
2012-04-28 20:53:41 -0400

    refactored

--- spec/yahoo/content/analyze/html_spec.rb
+++ spec/yahoo/content/analyze/html_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 module Yahoo
   module Content
     module Analyze
-      describe Links do
+      describe Html do
         ExpectedResult = [1297, 1298, 1299, 1308, 1302, 1303, 1309, 1314, 1316, 1315, 1317, 1320, 1322, 1323, 1336, 1340, 1342, 1353, 1366, 1451, 1452, 1453, 1455, 1461, 1463, 1466, 1486]
 
         let(:links) do
-          links = Links.new
+          html = Html.new
         end
 
         describe "#find" do
           it "should return an array of message ids" do
-            links.analyze( SampleHtml )
+            html.analyze( SampleHtml )
 
-            links.message_links.should == ExpectedResult
+            html.message_links.should == ExpectedResult
           end
         end
       end
