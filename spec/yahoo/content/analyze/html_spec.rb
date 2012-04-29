cec794362954e4db0da573d5b0a086567c942ce8
penumbra <penumbraposts@yahoo.com>
2012-04-28 20:55:37 -0400

    refactored

--- spec/yahoo/content/analyze/html_spec.rb
+++ spec/yahoo/content/analyze/html_spec.rb
@@ -6,7 +6,7 @@ module Yahoo
       describe Html do
         ExpectedResult = [1297, 1298, 1299, 1308, 1302, 1303, 1309, 1314, 1316, 1315, 1317, 1320, 1322, 1323, 1336, 1340, 1342, 1353, 1366, 1451, 1452, 1453, 1455, 1461, 1463, 1466, 1486]
 
-        let(:links) do
+        let(:html) do
           html = Html.new
         end
 
